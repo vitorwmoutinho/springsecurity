99aae574867c78fda42bb8083fe5d6cf1d7890d8
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-14 16:37:12 -0300

    Aprendendo Autenticação simples

---
+++ src/main/java/dio/spring/security/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package dio.spring.security.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig implements WebSecurityConfigurer {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+
+
+    }
+
+    @Override
+    public void init(SecurityBuilder builder) throws Exception {
+
+    }
+
+    @Override
+    public void configure(SecurityBuilder builder) throws Exception {
+
+    }
+}
