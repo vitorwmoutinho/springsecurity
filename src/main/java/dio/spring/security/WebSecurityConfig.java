66db25cc7df5c58a975b93c506711b61931a26ae
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-15 08:04:31 -0300

    Aprendendo sobre Autenticação com Banco de dados

--- src/main/java/dio/spring/security/WebSecurityConfig.java
+++ src/main/java/dio/spring/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package dio.spring.security.security;
+package dio.spring.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
